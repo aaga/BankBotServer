48d6db84b5c46932b391f867c22847a28cc99fa7
Ashwin Agarwal <aaga@stanford.edu>
2016-08-19 17:06:50 +0100

    debug

--- index.js
+++ index.js
@@ -94,7 +94,16 @@ const actions = {
       // Yay, we found our recipient!
       // Let's forward our bot response to her.
       // We return a promise to let our bot know when we're done sending
-      return fbMessage(recipientId, text, quickreplies);
+      return fbMessage(recipientId, text, quickreplies)
+      .then(() => null)
+      .catch((err) => {
+        console.error(
+          'Oops! An error occurred while forwarding the response to',
+          recipientId,
+          ':',
+          err.stack || err
+        );
+      });
     } else {
       console.error('Oops! Couldn\'t find user for session:', sessionId);
       // Giving the wheel back to our bot
@@ -183,6 +192,7 @@ app.post('/webhook', (req, res) => {
             // We received an attachment
             // Let's reply with an automatic message
             fbMessage(sender, 'Sorry I can only process text messages for now.')
+            .catch(console.error);
           } else if (text) {
             // We received a text message
 
